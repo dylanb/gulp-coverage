9604888ef4edcde79e71b4ac5c808a6ca89c686b
Dylan Barrell <dylan@barrell.com>
2014-01-19 15:18:51 -0500

    add some gulp targets to test the watch functionality

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,9 @@ gulp.task('debug', function () {
     });
 });
 
+/*
+ * these tasks are to actually use the plugin and test it within the context of gulp
+ */
 gulp.task('blnkt', function () {
     gulp.src(['src.js', 'src2.js', 'src3.js'], { read: false })
         .pipe(cover.instrument({
@@ -45,16 +48,9 @@ gulp.task('blnkt', function () {
         }));
 });
 
-gulp.task('blnkt2', function () {
-    gulp.src(['src2.js', 'src3.js'], { read: false })
-        .pipe(cover.instrument({
-            pattern: ['**/test*'],
-            debugDirectory: 'debug'
-        }))
-        .pipe(mocha({
-        }))
-        .pipe(cover.report({
-            outFile: 'blnkt2.html'
-        }));
+gulp.task('watch', function () {
+    gulp.watch(['src.js', 'src2.js', 'src3.js', 'test.js', 'test2.js'], function(event) {
+      gulp.run('blnkt');
+    });    
 });
 
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ module.exports = function () {
     var i, matcher,
         retVal = 0;
 
+    // throw new Error('bugger');
     for (i = 0; i < 10; i++) {
         matcher = Math.floor(Math.random()*10+0.5);
         if (matcher === i) {
