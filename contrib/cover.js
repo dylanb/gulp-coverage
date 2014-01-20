7206b9ba90f5dd642fe61f0beb6d75bda7ee1c3a
Dylan Barrell <dylan@barrell.com>
2014-01-19 22:45:09 -0500

    Implement all the new coverage stat code

--- contrib/cover.js
+++ contrib/cover.js
@@ -8,48 +8,39 @@ var multimatch = require('multimatch');
 
 // Coverage tracker
 function CoverageData (filename, instrumentor) {
+    var theLines = {};
     this.instrumentor = instrumentor;
     this.filename = filename;
     this.nodes = {};
     this.visitedBlocks = {};
     this.source = instrumentor.source;
+    Object.keys(instrumentor.nodes).forEach(function(index) {
+        var node = instrumentor.nodes[index],
+            lineStruct;
+
+        if (!theLines[node.loc.start.line]) {
+            lineStruct = theLines[node.loc.start.line] = {
+                nodes: []
+            };
+        } else {
+            lineStruct = theLines[node.loc.start.line];
+        }
+        if (lineStruct.nodes.indexOf(node) === -1) {
+            lineStruct.nodes.push(node);
+        }
+        if (!theLines[node.loc.end.line]) {
+            lineStruct = theLines[node.loc.end.line] = {
+                nodes: []
+            };
+        } else {
+            lineStruct = theLines[node.loc.end.line];
+        }
+        if (lineStruct.nodes.indexOf(node) === -1) {
+            lineStruct.nodes.push(node);
+        }
+    });
+    this.lines = theLines;
 }
-
-// Note that a node has been visited
-CoverageData.prototype.visit = function(node) {
-    this.nodes[node.id] = (this.nodes[node.id] || { node : node, count : 0});
-
-    this.nodes[node.id].count++;
-};
-
-// Note that a node has been visited
-CoverageData.prototype.visitBlock = function(blockIndex) {
-    var block = this.visitedBlocks[blockIndex] = (this.visitedBlocks[blockIndex] || {count:0});
-
-    block.count++;
-};
-
-// Get all the nodes we did not see
-CoverageData.prototype.missing = function() {
-    // Find all the nodes which we haven't seen
-    var nodes = this.nodes;
-    var missing = this.instrumentor.filter(function(node) {
-        return !nodes[node.id];
-    });
-
-    return missing;
-};
-
-// Get all the nodes we did see
-CoverageData.prototype.seen = function() {  
-    // Find all the nodes we have seen
-    var nodes = this.nodes;
-    var seen = this.instrumentor.filter(function(node) {
-        return !!nodes[node.id];
-    });
-    
-    return seen;
-};
 
 // Calculate node coverage statistics
 CoverageData.prototype.blocks = function() {
@@ -57,148 +48,12 @@ CoverageData.prototype.blocks = function() {
     var numSeenBlocks = 0;
     for(var index in this.visitedBlocks) {
         numSeenBlocks++;
-    }
-    var numMissingBlocks = totalBlocks - numSeenBlocks;
-    
+    }    
     var toReturn = {
         total: totalBlocks,
-        seen: numSeenBlocks,
-        missing: numMissingBlocks,
-        percentage: totalBlocks ? numSeenBlocks / totalBlocks : 1
+        seen: numSeenBlocks
     };
-    
     return toReturn;
-};
-
-// Explode all multi-line nodes into single-line ones.
-var explodeNodes = function(coverageData, fileData) {  
-    var missing = coverageData.missing(); 
-    var newNodes = [];
-    
-    // Get only the multi-line nodes.
-    var multiLineNodes = missing.filter(function(node) {
-        return (node.loc.start.line < node.loc.end.line);
-    });
-    
-    for(var i = 0; i < multiLineNodes.length; i++) {
-        // Get the current node and delta
-        var node = multiLineNodes[i];
-        var lineDelta = node.loc.end.line - node.loc.start.line + 1;
-        
-        for(var j = 0; j < lineDelta; j++) {
-            // For each line in the multi-line node, we'll create a 
-            // new node, and we set the start and end columns
-            // to the correct vlaues.
-            var curLine = node.loc.start.line + j;
-            var startCol = 0;
-            var endCol = fileData[curLine - 1].length;
-                
-            if (curLine === node.loc.start.line) {
-                startCol = node.loc.start.column;
-            }
-            else if (curLine === node.loc.end.line) {
-                startCol = 0;
-                endCol = node.loc.end.column;
-            }
-            
-            var newNode = {
-                loc:
-                    {
-                        start: {
-                            line: curLine,
-                            col: startCol
-                        },
-                        end: {
-                            line: curLine,
-                            col: endCol
-                        }
-                    }
-            };
-            
-            newNodes.push(newNode);
-        }
-    }
-    
-    return newNodes;
-};
-
-// Get per-line code coverage information
-CoverageData.prototype.coverage = function() {  
-    var missingLines = this.missing();
-    var fileData = this.instrumentor.source.split('\n');
-    
-    // Get a dictionary of all the lines we did observe being at least
-    // partially covered
-    var seen = {};
-    
-    this.seen().forEach(function(node) {
-        seen[node.loc.start.line] = true;
-    });
-    
-    // Add all the new multi-line nodes.
-    missingLines = missingLines.concat(explodeNodes(this, fileData));
-    
-    var seenNodes = {};
-    missingLines = missingLines.sort(
-        function(lhs, rhs) {
-          var lhsNode = lhs.loc;
-          var rhsNode = rhs.loc;
-          
-          // First try to sort based on line
-          return lhsNode.start.line < rhsNode.start.line ? -1 : // first try line
-                 lhsNode.start.line > rhsNode.start.line ? 1  :
-                 lhsNode.start.column < rhsNode.start.column ? -1 : // then try start col
-                 lhsNode.start.column > rhsNode.start.column ? 1 :
-                 lhsNode.end.column < rhsNode.end.column ? -1 : // then try end col
-                 lhsNode.end.column > rhsNode.end.column ? 1 : 
-                 0; // then just give up and say they are equal
-    }).filter(function(node) {
-        // If it is a multi-line node, we can just ignore it
-        if (node.loc.start.line < node.loc.end.line) {
-            return false;
-        }
-        
-        // We allow multiple nodes per line, but only one node per
-        // start column (due to how instrumented works)
-        var okay = false;
-        if (seenNodes.hasOwnProperty(node.loc.start.line)) {
-            var isNew = (seenNodes[node.loc.start.line].indexOf(node.loc.start.column) < 0);
-            if (isNew) {
-                seenNodes[node.loc.start.line].push(node.loc.start.column);
-                okay = true;
-            }
-        }
-        else {
-            seenNodes[node.loc.start.line] = [node.loc.start.column];
-            okay = true;
-        }
-        
-        return okay;
-    });
-    
-    var coverage = {};
-    
-    missingLines.forEach(function(node) {
-        // For each missing line, add some information for it
-        var line = node.loc.start.line;
-        var startCol = node.loc.start.column;
-        var endCol = node.loc.end.column;
-        var source = fileData[line - 1];
-        var partial = seen.hasOwnProperty(line) && seen[line];
-        
-        if (coverage.hasOwnProperty(line)) {
-            coverage[line].missing.push({startCol: startCol, endCol: endCol});
-        }
-        else {
-            coverage[line] = {
-                  partial: partial,
-                  source: source,
-                  missing: [{startCol: startCol, endCol: endCol}]
-            };
-      }
-    });
-    
-    return coverage;
 };
 
 CoverageData.prototype.prepare = function() {
@@ -228,7 +83,7 @@ CoverageData.prototype.prepare = function() {
 
     for (index in store.nodes) {
         if (store.nodes.hasOwnProperty(index)) {
-            this.nodes[index] = {node: this.instrumentor.nodes[index], count: store.nodes[index].count};
+            this.instrumentor.nodes[index].count = store.nodes[index].count;
         }
     }
     
@@ -239,51 +94,93 @@ CoverageData.prototype.prepare = function() {
     }
 };
 
-// Get statistics for the entire file, including per-line code coverage
-// and block-level coverage
+/**
+ * 
+ * Get statistics for the entire file, including per-line code coverage
+ * statement coverage and block-level coverage
+ * This function returns an object with the following structure:
+ * {
+ *      lines: Integer - the number of lines covered
+ *      blocks: Integer - the number of blocks covered
+ *      statements: Integer - the number of statements covered
+ *      lineDetails: Array[Object] - a sparse array of the detailed information on each line
+ *      sloc: Integer - the number of relevant lines in the file
+ *      sboc: Integer - the number of relevant blocks in the file
+ *      ssoc: Integer - the number of relevant statements in the file
+ *      code: Array[String] - an Array of strings, one for each line of the file
+ * }
+ *
+ * The line detail objects have the following structure
+ * {
+ *      number: Integer - the line number
+ *      count: Integer - the number of times the line was executed
+ *      statements: Integer - the number of statements covered
+ *      ssoc: Integer - the number of statements in the line
+ *      statementDetails : Array[Object] - an array of the statement details
+ * }
+ *
+ * The statement detail objects have the following structure
+ * {
+ *      loc: Object - a location object
+ *      count: the number of times the statement was executed
+ * }
+ *
+ */
+
 CoverageData.prototype.stats = function() {
     this.prepare();
-    
-    var missing = this.missing();
-    var filedata = this.instrumentor.source.split('\n');
-    
-    var observedMissing = [];
-    var linesInfo = missing.sort(function(lhs, rhs) {
-        return lhs.loc.start.line < rhs.loc.start.line ? -1 :
-               lhs.loc.start.line > rhs.loc.start.line ? 1  :
-               0;
-        }).filter(function(node) {
-            // Make sure we don't double count missing lines due to multi-line
-            // issues
-            var okay = (observedMissing.indexOf(node.loc.start.line) < 0);
-            if(okay) {
-              observedMissing.push(node.loc.start.line);
+        var filedata = this.instrumentor.source.split('\n');
+    var lineDetails = [],
+        lines = 0, fileStatements = 0, fileSsoc = 0, fileSloc = 0,
+        theLines = this.lines,
+        blockInfo;
+
+    Object.keys(theLines).forEach(function(index) {
+        var line = theLines[index],
+            lineStruct,
+            lineCount = 0,
+            statements = 0,
+            ssoc = 0,
+            statementDetails = [];
+        line.nodes.forEach(function(node) {
+            if (node.count === null || node.count === undefined) {
+                node.count = 0;
             }
-            
-            return okay;
-        }).map(function(node, idx, all) {
-            // For each missing line, add info for it
-            return {
-                lineno: node.loc.start.line,
-                source: function() { return filedata[node.loc.start.line - 1]; }
-            };
+            lineCount = Math.max(lineCount, node.count);
+            ssoc += 1;
+            if (node.count) {
+                statements += 1;
+            }
+            statementDetails.push({
+                loc: node.loc,
+                count: node.count
+            });
         });
-        
-    var numLines = filedata.length;
-    var numMissingLines = observedMissing.length;
-    var numSeenLines = numLines - numMissingLines;
-    var percentageCovered = numSeenLines / numLines;
-        
-    return {
-        percentage: percentageCovered,
-        lines: linesInfo,
-        missing: numMissingLines,
-        seen: numSeenLines,
-        total: numLines,
-        coverage: this.coverage(),
-        source: this.source,
-        blocks: this.blocks()
+        lineStruct = {
+            number: index,
+            count: lineCount,
+            ssoc: ssoc,
+            statements: statements,
+            statementDetails: statementDetails
+        };
+        lines += (lineStruct.count ? 1 : 0);
+        fileSloc += 1;
+        fileStatements += lineStruct.statements;
+        fileSsoc += lineStruct.ssoc;
+        lineDetails[index-1] = lineStruct;
+    });
+    blockInfo = this.blocks();
+    retVal = {
+        lines: lines,
+        statements: fileStatements,
+        blocks: blockInfo.seen,
+        sloc: fileSloc,
+        ssoc: fileSsoc,
+        sboc: blockInfo.total,
+        lineDetails: lineDetails,
+        code: filedata
     };
+    return retVal;
 };
 
 var addInstrumentationHeader = function(template, filename, instrumented, coverageStorePath) {
@@ -305,57 +202,6 @@ var stripBOM = function(content) {
         content = content.slice(1);
     }
     return content;
-};
-
-var load = function(datas) {
-    var combinedCoverage = {};
-    
-    _.each(datas, function(data) {  
-        _.each(data.files, function(fileData, filename) {
-            var instrumentor = {
-                source: fileData.instrumentor.source,
-                nodes: fileData.instrumentor.nodes,
-                nodeCounter: fileData.instrumentor.nodeCounter,
-                blockCounter: fileData.instrumentor.blockCounter,
-            };
-            
-            var coverage = new CoverageData(filename, new instrument.Instrumentor());
-            coverage.instrumentor.source = fileData.instrumentor.source;
-            coverage.instrumentor.nodes = fileData.instrumentor.nodes;
-            coverage.instrumentor.nodeCounter = fileData.instrumentor.nodeCounter;
-            coverage.instrumentor.blockCounter = fileData.instrumentor.blockCounter;
-            
-            coverage.source = coverage.instrumentor.source;
-            coverage.hash = fileData.hash;
-            coverage.nodes = fileData.nodes;
-            coverage.visitedBlocks = fileData.blocks;
-            
-            if (combinedCoverage.hasOwnProperty(filename)) {
-                var coverageSoFar = combinedCoverage[filename];
-                
-                if (coverageSoFar.hash !== coverage.hash) {
-                    throw new Error("Multiple versions of file '" + filename + "' in coverage data!");
-                }
-                
-                // Merge nodes
-                _.each(coverage.nodes, function(nodeData, index) {
-                    var nodeDataSoFar = coverageSoFar.nodes[index] = (coverageSoFar.nodes[index] || {node: nodeData.node, count: 0});
-                    nodeDataSoFar.count += nodeData.count;
-                });
-                
-                // Merge blocks
-                _.each(coverage.visitedBlocks, function(blockData, index) {
-                    var blockDataSoFar = coverageSoFar.visitedBlocks[index] = (coverageSoFar.visitedBlocks[index] || {count: 0});
-                    blockDataSoFar.count += blockData.count;
-                });
-            }
-            else {
-                combinedCoverage[filename] = coverage;
-            }
-        });
-    });
-    
-    return combinedCoverage;
 };
 
 var cover = function(pattern, debugDirectory) {    
@@ -449,7 +295,6 @@ module.exports = {
     cover: cover,
     init: init,
     cleanup: cleanup,
-    load: load,
     reporters: {
         html:   require('./reporters/html')
     }
