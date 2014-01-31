139584fc55473af1376b1002132de1e5c439e184
Dylan Barrell <dylan@barrell.com>
2014-01-31 08:15:03 -0500

    refactor the gulpfile to support serial tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,52 @@
 var gulp = require('gulp'),
     cover = require('./index'),
-    mocha = require('gulp-mocha'),
+    mochaTask = require('gulp-mocha'),
     jshint = require('gulp-jshint'),
     exec = require('child_process').exec,
-    jasmine = require('gulp-jasmine');
+    jasmineTask = require('gulp-jasmine'),
+    through2 = require('through2');
 
-gulp.task('test', function () {
+/*
+ * Define the dependency arrays
+ */
+
+var lintDeps = [],
+    testDeps = [],
+    debugDeps = [],
+    mochaDeps = [],
+    jsonDeps = [],
+    jasmineDeps = [],
+    testchainDeps = []
+
+/*
+ * Define the task functions
+ */
+
+function synchro (done) {
+    return through2.obj(function (data, enc, cb) {
+        cb();
+    },
+    function (cb) {
+        cb();
+        done();
+    });
+}
+
+function test () {
     gulp.src(['test/**.js'], { read: false })
-        .pipe(mocha({
+        .pipe(mochaTask({
             reporter: 'spec'
         }));
-});
+}
 
-gulp.task('lint', function () {
+function lint (done) {
     gulp.src(['test/**/*.js', 'index.js', 'contrib/cover.js', 'contrib/coverage_store.js', 'contrib/reporters/**/*.js'])
         .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-});
+        .pipe(jshint.reporter('default')
+        .pipe(synchro(done)));
+}
 
-gulp.task('debug', function () {
+function debug (cb) {
     exec('node --debug-brk debug/chaindebug.js', {}, function (error, stdout, stderr) {
         console.log('STDOUT');
         console.log(stdout);
@@ -29,74 +57,100 @@ gulp.task('debug', function () {
             console.log(error);
         }
     });
-});
+    cb();
+}
 
-/*
- * these tasks are to actually use the plugin and test it within the context of gulp
- */
-
-gulp.task('default', function(){
-  gulp.run('mocha', 'json', 'jasmine', 'testchain', 'test', 'lint');
-});
-
-gulp.task('mocha', function () {
+function mocha (done) {
     gulp.src(['testsupport/src.js', 'testsupport/src3.js'], { read: false })
         .pipe(cover.instrument({
             pattern: ['**/test*'],
             debugDirectory: 'debug'
         }))
-        .pipe(mocha({
+        .pipe(mochaTask({
             reporter: 'spec'
         }))
         .pipe(cover.report({
             outFile: 'testoutput/blnkt.html'
-        }));
-});
+        })
+        .pipe(synchro(done)));
+}
 
-gulp.task('json', function () {
-    gulp.src(['testsupport/src.js', 'vsrc3.js'], { read: false })
+function json (done) {
+    gulp.src(['testsupport/src.js', 'testsupprt/src3.js'], { read: false })
         .pipe(cover.instrument({
-            pattern: ['**/test*']
+            pattern: ['**/test*'],
+            debugDirectory: 'debug'
         }))
-        .pipe(mocha({
+        .pipe(mochaTask({
             reporter: 'spec'
         }))
         .pipe(cover.report({
             reporter: 'json',
             outFile: 'testoutput/json.json'
-        }));
-});
+        })
+        .pipe(synchro(done)));
+}
 
-gulp.task('jasmine', function () {
+function jasmine (done) {
     gulp.src('testsupport/srcjasmine.js')
         .pipe(cover.instrument({
-            pattern: ['**/test*'],
-            debugDirectory: 'debug'
+            pattern: ['**/test*']
         }))
-        .pipe(jasmine())
+        .pipe(jasmineTask())
         .pipe(cover.report({
             outFile: 'testoutput/jasmine.html'
-        }));
+        })
+        .pipe(synchro(done)));
+}
+
+function testchain (done) {
+    gulp.src(['testsupport/srcchain.js'], { read: false })
+        .pipe(cover.instrument({
+            pattern: ['**/chain.js'],
+            debugDirectory: 'debug'
+        }))
+        .pipe(mochaTask({
+            reporter: 'spec'
+        }))
+        .pipe(cover.report({
+            outFile: 'testoutput/chain.html'
+        })
+        .pipe(synchro(done)));
+}
+
+/*
+ * setup function
+ */
+
+function setup () {
+    gulp.task('test', testDeps, test);
+    gulp.task('lint', lintDeps, lint);
+    gulp.task('mocha', mochaDeps, mocha);
+    gulp.task('json', jsonDeps, json);
+    gulp.task('jasmine', jasmineDeps, jasmine);
+    gulp.task('testchain', testchainDeps, testchain);    
+}
+
+/*
+ * Actual task defn
+ */
+
+gulp.task('default', function() {
+    // Setup the chain of dependencies
+    jasmineDeps = ['testchain'];
+    jsonDeps = ['jasmine'];
+    mochaDeps = ['json'];
+    testDeps = ['mocha'];
+    setup();
+    gulp.run('test');
 });
+
+gulp.task('debug', debugDeps, debug);
+
+setup();
 
 gulp.task('watch', function () {
     gulp.watch(['testsupport/src.js', 'testsupport/src3.js', 'test.js', 'test2.js'], function(event) {
       gulp.run('mocha');
     });    
 });
-
-gulp.task('testchain', function () {
-    gulp.src(['testsupport/srcchain.js'], { read: false })
-        .pipe(cover.instrument({
-            pattern: ['**/chain.js'],
-            debugDirectory: 'debug'
-        }))
-        .pipe(mocha({
-            reporter: 'spec'
-        }))
-        .pipe(cover.report({
-            outFile: 'testoutput/chain.html'
-        }));
-});
-
-
