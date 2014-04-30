161b692b3a569ace131fd74e6caf00cc20a06e8d
Dylan Barrell <dylan@barrell.com>
2014-04-29 21:13:05 -0400

    make the __instrumented_miss attribute non-enumerable so it does not mess up deepEqual comparisons

--- testsupport/chain.js
+++ testsupport/chain.js
@@ -9,5 +9,6 @@ module.exports = function () {
         10+
         0.5
     );
+    return chain;
 };
 
--- testsupport/srcchain.js
+++ testsupport/srcchain.js
@@ -2,8 +2,14 @@ var assert = require('assert'),
     test = require('./chain');
 
 describe('Test Src', function () {
-    it('Should run this test Src', function () {
-        test();
-        assert.ok(true);
+    it('Should run this test for the chainable and not find an enumerable __instrumented_miss attribute on the object', function () {
+        var chain = test(),
+            props = [],
+            i;
+        for(i in chain) {
+            props[i] = true;
+        }
+        assert.ok(chain.hasOwnProperty('__instrumented_miss'));
+        assert.ok(!props['__instrumented_miss']);
     });
 });
